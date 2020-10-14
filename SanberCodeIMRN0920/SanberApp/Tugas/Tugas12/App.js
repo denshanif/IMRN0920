1b382f9db1eb05fa029e768db224d8bb2db40e49
denshanif <alfatoni922@gmail.com>
2020-10-14 10:20:09 +0700

    update tugas 12 (1)

--- SanberCodeIMRN0920/SanberApp/Tugas/Tugas12/App.js
+++ SanberCodeIMRN0920/SanberApp/Tugas/Tugas12/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons'
+import { Platform, View, Text, StyleSheet, Image, TouchableOpacity, FlatList } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import VideoItem from './components/videoitem';
+import data from './data.json';
 
 export default class App extends Component {
     render() {
@@ -18,10 +20,30 @@ export default class App extends Component {
                     </View>
                 </View>
                 <View style={styles.body}>
-
+                    <FlatList
+                    data={data.items}
+                    renderItem={(video) => <VideoItem video={video.item} />}
+                    keyExtractor={(item) => item.id}
+                    ItemSeparatorComponent={()=><View style={{height:0.5,backgroundColor:"#e5e5e5"}} />}
+                    />
                 </View>
                 <View style={styles.tabBar}>
-
+                    <TouchableOpacity style={styles.tabItem}>
+                        <Icon name="home" size={25} />
+                        <Text style={styles.tabTitle}>Home</Text> 
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.tabItem}>
+                        <Icon name="whatshot" size={25} />
+                        <Text style={styles.tabTitle}>Trending</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.tabItem}>
+                        <Icon name="subscriptions" size={25} />
+                        <Text style={styles.tabTitle}>Subscriptions</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.tabItem}>
+                        <Icon name="folder" size={25} />
+                        <Text style={styles.tabTitle}>Library</Text>
+                    </TouchableOpacity>
                 </View> 
             </View>
         )
@@ -54,6 +76,17 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         height: 60,
         borderTopWidth: 0.5,
-        borderColor: '#E5E5E5'
+        borderColor: '#E5E5E5',
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    tabItem: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    tabTitle: {
+        fontSize: 11,
+        color: '#3c3c3c',
+        paddingTop: 4
     }
 })
